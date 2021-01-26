2fcd9fb6065c6898d99f7a7b65a16c6e3fd385fb
Ken Ren <kengrand369@gmail.com>
2021-01-25 23:59:09 -0600

    fed not fed logic

--- APP1/src/checker.js
+++ APP1/src/checker.js
@@ -1,5 +1,5 @@
-const STATUS_NOT_FED = "STATUS_NOT_FED"
-const STATUS_FED = "STATUS_FED"
+const STATUS_NOT_FED = "NOT_FED"
+const STATUS_FED = "FED"
 
 const notFedImage = "https://icon-library.com/images/uncheck-icon/uncheck-icon-17.jpg";
 const fedImage = "https://upload.wikimedia.org/wikipedia/commons/e/e0/Check_green_icon.svg";
@@ -8,8 +8,15 @@ const feedCards = document.querySelectorAll(".feed-status");
 for(const card of feedCards){
     card.addEventListener("click",function(){
         let currentStatus = card.dataset.status
+
+        /*currentStatus.addEventListener("change",function(){
+            let feedTime = new Date()
+            console.log(feedTime)
+        })*/
+        console.log(currentStatus)
         currentStatus = currentStatus === STATUS_FED ? STATUS_NOT_FED : STATUS_FED
-        //card.dataset.status = currentStatus//
+        //if not fed will be fed, if fed become not fed//
+        console.log(currentStatus)
         const image = card.querySelector(".feed-status__img")
         image.src = currentStatus === STATUS_FED ? fedImage : notFedImage  
     })
