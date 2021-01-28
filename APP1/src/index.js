b51350719c71196112f31b18c954631ba7ad1884
Ken Ren <kengrand369@gmail.com>
2021-01-28 16:27:17 -0600

    ask an

--- APP1/src/index.js
+++ APP1/src/index.js
@@ -3,19 +3,27 @@ var nameSelected = false;
 function onNameButtonClick(name) {
   nameSelected = true;
   console.log(name);
+  return name
   //use input instead of 
   //if logged in,jump to checker page
   //to do save name to localstorage
 }
-function onNameInput() {
-  nameSelected = true;
-  const x = document.getEslementById("nameInput").value;
-  console.log(x);
-}
 
 function onSignIn() {
   if (nameSelected) {
+    const feeder =
     window.location.href = "checker.html";
+
+  } else if(document.getElementById("NameInput").value !== null){
+    const feeder = document.getElementById("NameInput").value
+    console.log(feeder);
+    /*window.location.href = "checker.html"; 
+    localStorage.setItem("Feeder",feeder)
+    var jsonString = localStorage.getItem("Feeder");
+    var retrievedObject = JSON.parse(jsonString);
+    console.log(retrievedObject);*/
+
+
   } else {
     alert("Please select your name!");
   }
